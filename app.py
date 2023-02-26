3dc893ecb3b00439c1482d8bbb6941a0a56399bc
Igor Nest <druidd95@gmail.com>
2023-02-26 06:41:55 +0200

    Added blogs write func

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template, flash, session, request, redirect
 from flask_bootstrap import Bootstrap
 from flask_mysqldb import MySQL
+from flask_ckeditor import CKEditor
 from werkzeug.security import generate_password_hash, check_password_hash
 import yaml, os
 
 
 app = Flask(__name__)
 Bootstrap(app)
+CKEditor(app)
 
 db = yaml.load(open("db.yaml"), Loader=yaml.FullLoader)
 app.config["MYSQL_HOST"] = db["mysql_host"]
@@ -20,7 +22,13 @@ mysql = MySQL(app)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    cursor = mysql.connection.cursor()
+    result_value = cursor.execute("SELECT * FROM blog")
+    if result_value > 0:
+        blogs = cursor.fetchall()
+        cursor.close()
+        return render_template("index.html", blogs=blogs)
+    return render_template("index.html", blogs=None)
 
 
 @app.route("/about/")
@@ -30,7 +38,12 @@ def about():
 
 @app.route("/blogs/<int:id>")
 def blogs(id):
-    return render_template("blogs.html", blog_id=id)
+    cursor = mysql.connection.cursor()
+    result_value = cursor.execute(f"SELECT * FROM blog WHERE blog_id = {id}")
+    if result_value > 0:
+        blog = cursor.fetchone()
+        return render_template("blogs.html", blog=blog)
+    return "Blog is not found"
 
 
 @app.route("/register/", methods=["GET", "POST"])
@@ -89,6 +102,16 @@ def login():
 
 @app.route("/write-blog", methods=["GET", "POST"])
 def write_blog():
+    if request.method == "POST":
+        blogpost = request.form
+        title = blogpost["title"]
+        body = blogpost["body"]
+        author = session["first_name"] + " " + session["last_name"]
+        cursor = mysql.connection.cursor()
+        cursor.execute("INSERT INTO blog (title, body, author) VALUES (%s, %s, %s)", (title, body, author))
+        mysql.connection.commit()
+        flash("Your blogpost is successfully posted!", "success")
+        return redirect("/")
     return render_template("write-blog.html")
 
 
