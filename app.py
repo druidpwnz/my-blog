74f33241c4a43a29798bae26f0ef6176d58d9c79
Igor Nest <druidd95@gmail.com>
2023-02-26 15:53:28 +0200

    Added editblogs/logout menu

--- app.py
+++ app.py
@@ -117,22 +117,50 @@ def write_blog():
 
 @app.route("/my-blogs/")
 def my_blogs():
-    return render_template("my-blogs.html")
+    author = session["first_name"] + " " + session["last_name"]
+    cursor = mysql.connection.cursor()
+    result_value = cursor.execute("SELECT * FROM blog WHERE author = %s", [author])
+    if result_value > 0:
+        my_blogs = cursor.fetchall()
+        return render_template("my-blogs.html", my_blogs=my_blogs)
+    else:
+        return render_template("my-blogs.html", my_blogs=None)
 
 
 @app.route("/edit-blog/<int:id>", methods=["GET", "POST"])
 def edit_blog(id):
-    return render_template("edit-blog.html", blog_id=id)
+    if request.method == "POST":
+        cursor = mysql.connection.cursor()
+        title = request.form["title"]
+        body = request.form["body"]
+        cursor.execute("UPDATE blog SET title = %s, body = %s WHERE blog_id = %s", (title, body, id))
+        mysql.connection.commit()
+        flash("Blog is updated successfully", "success")
+        return redirect(f"/blogs/{id}")
+    cursor = mysql.connection.cursor()
+    result_value = cursor.execute(f"SELECT * FROM blog WHERE blog_id = {id}")
+    if result_value> 0:
+        blog = cursor.fetchone()
+        blog_form = {}
+        blog_form["title"] = blog["title"]
+        blog_form["body"] = blog["body"]
+        return render_template("edit-blog.html", blog_form=blog_form)
 
 
-@app.route("/delete-blog/<int:id>", methods=["POST"])
+@app.route("/delete-blog/<int:id>")
 def delete_blog(id):
-    return "Succesfully Delete"
+    cursor = mysql.connection.cursor()
+    cursor.execute(f"DELETE FROM blog WHERE blog_id = {id}")
+    mysql.connection.commit()
+    flash("Your blog has been deleted", "success")
+    return redirect("/my-blogs")
 
 
 @app.route("/logout/")
 def logout():
-    return render_template("logout.html")
+    session.clear()
+    flash("You have been logged out", "info")
+    return redirect("/") 
 
 
 if __name__ == "__main__":
